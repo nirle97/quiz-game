12b27838bc38da408b8a43988a5fc88fbed78049
Ben Hanover <benhanover4@gmail.com>
2021-04-23 16:34:56 +0300

    basic css and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,30 @@ import ScoreBoard from './components/ScoreBoard';
 import ErrorBoundary from './components/ErrorBoundary';
 
 export const PlayerContext = createContext();
+
 function App() {
   const [playerName, setPlayerName] = useState('');
   return (
     <ErrorBoundary>
       <PlayerContext.Provider value={[playerName, setPlayerName]}>
-        <Router>
-          <div>
-            <Switch>
-              <Route path='/' exact component={Lobby} />
-              <Route
-                path='/game'
-                render={(props) => <Game {...props} playerName={playerName} />}
-              />
-              <Route path='/scoreboard' component={ScoreBoard} />
-            </Switch>
+        <div className='board-outside-div'>
+          <div className='board-inside-div'>
+            <h1 className='title'>QUIZ GAME</h1>
+
+            <Router>
+              <Switch>
+                <Route path='/' exact component={Lobby} />
+                <Route
+                  path='/game'
+                  render={(props) => (
+                    <Game {...props} playerName={playerName} />
+                  )}
+                />
+                <Route path='/scoreboard' component={ScoreBoard} />
+              </Switch>
+            </Router>
           </div>
-        </Router>
+        </div>
       </PlayerContext.Provider>
     </ErrorBoundary>
   );
--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,11 +1,18 @@
-import React, { useRef, useContext } from 'react';
+import React, { useRef, useContext, useState } from 'react';
 import { PlayerContext } from '../App';
 import '../styles/lobby.css';
 
 function Lobby({ history }) {
   const playerName = useRef(null);
   const [userName, setUserName] = useContext(PlayerContext);
+  const [loginError, setLoginError] = useState('');
+
+  //functions
   function startGame() {
+    if (!playerName.current) {
+      setLoginError('Cannot start the game with a blank name');
+      return;
+    }
     setUserName(playerName.current);
     history.push('/game');
   }
@@ -15,13 +22,18 @@ function Lobby({ history }) {
 
   return (
     <>
-      <h1>Welcome to the QUIZ GAME</h1>
-      <input
-        type='text'
-        onChange={(e) => (playerName.current = e.target.value)}
-      />
-      <button onClick={() => startGame()}>Start</button>
-      <button onClick={() => goToScoreBoard()}>ScoreBoard</button>
+      <div className='input-and-start-button-div'>
+        <input
+          type='text'
+          placeholder='What is your name?'
+          onChange={(e) => (playerName.current = e.target.value)}
+        />
+        <button onClick={() => startGame()}>Start</button>
+      </div>
+      <button className='score-board' onClick={() => goToScoreBoard()}>
+        ScoreBoard
+      </button>
+      <div>{loginError}</div>
     </>
   );
 }
